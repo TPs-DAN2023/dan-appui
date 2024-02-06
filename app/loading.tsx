9f56ef951f3288fab7a2ba049544d5bf9afd94bf
ChortJulio <chortjulio@gmail.com>
2024-02-06 13:39:55 -0300

    Add Loading component

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <>
+    <section className="flex justify-center">
+      <h1>Loading...</h1>
+    </section>
+    </>
+  );
+}
