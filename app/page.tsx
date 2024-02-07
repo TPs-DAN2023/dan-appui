2ab9debe8c963efb5db2ef7bdfab2df5b694f731
ChortJulio <chortjulio@gmail.com>
2024-02-07 13:51:20 -0300

    Refactor page component and add login functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,19 @@
 'use client'
 
-import Button from "@/components/Button";
 import CustomButton from "@/components/CustomButton";
-import { useState } from "react";
+import Login from "@/components/Login";
 
 export default function Home() {
 
-  const [estadoMicroServicio,setEstadoMicroservicio] = useState({})
-
-  const checkMicroService = async (msURL: string | URL | Request) => {
-    // Fetch data from external API
-    const res = await fetch(msURL);
-    const data = await res.json();
-    if (!data) {
-      return {
-        notFound: true
-      };
-    }
-    console.log(data);
-    setEstadoMicroservicio(data); 
-  }
-
-
   return (
     <>
-      <h1 className="flex justify-center">Elegir a que MS quiere pegarle</h1>
-      <section className="flex gap-4 justify-center mt-5">
-        <Button onClick={() => {checkMicroService('http://localhost/api/usuarios')}}>Check Usuarios</Button>
-        <Button onClick={() => {checkMicroService('http://localhost/api/productos')}}>Check Productos</Button>
-        <Button onClick={() => {checkMicroService('http://localhost/api/pedidos')}}>Check Pedidos</Button>
+      <main className="relative h-screen flex-col flex items-center justify-center">
+        <Login />
+      <section className="absolute bottom-4 right-4 flex flex-col gap-y-2 items-center">
+        <h1>Hacer testing de los MS</h1>
+        <CustomButton text="Testear MS" href="/test" />
       </section>
-      <span>{ estadoMicroServicio && JSON.stringify(estadoMicroServicio) }</span>
-      <h1 className="flex justify-center">Prueba de redirección en el front</h1>
-      <section className="flex gap-4 justify-center mt-5">
-        <CustomButton text="ms-usuarios (TP-1)" href="/usuarios" />
-        <CustomButton text="ms-productos (TP-2)" href="/productos" />
-        <CustomButton text="ms-pedidos (TP-3)" href="/pedidos" />
-      </section>
+      </main>
     </>
   );
 }
