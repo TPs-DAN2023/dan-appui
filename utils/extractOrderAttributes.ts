74987251545e037a0311403ee357cfc609e88e75
ChortJulio <chortjulio@gmail.com>
2024-02-15 15:21:31 -0300

    Update extractAttributes functions to change the UI of the item element

--- utils/extractOrderAttributes.ts
+++ utils/extractOrderAttributes.ts
@@ -4,9 +4,9 @@ import { formatDate } from "./index"
 export default function extractOrderAttributes (order: IOrder) {
   const attributes = {title: '', body: [''], footer: '', status: ''};
   attributes.title = `ID pedido: ${order.id}`;
-  attributes.body = [`${order.detallePedido && order.detallePedido.length} productos`, `Precio total: $${order.detallePedido && order.detallePedido.reduce((acc, item) => acc + item.precio, 0)}`];
-  attributes.footer = `Fecha: ${formatDate(order.fecha)}`;
-  attributes.status = `${order.estados && order.estados[0]}`
+  attributes.body = [`Cantidad de productos: ${order.detallePedido && order.detallePedido.length}`, `Precio total: $${order.detallePedido && order.detallePedido.reduce((acc, item) => acc + item.precio, 0)}`, `Proveedor: ${order.proveedor.nombre} (id=${order.proveedor.id})`, `Usuario: ${order.user.userName} (id=${order.user.id})`, `Fecha: ${formatDate(order.fecha)}`];
+  attributes.footer = `Observaciones: ${order.observaciones}`;
+  attributes.status = `${order.estados && order.estados[order.estados.length - 1].estado	}`
 
   return attributes;
 }
--- utils/extractProductAttributes.ts
+++ utils/extractProductAttributes.ts
@@ -2,9 +2,9 @@ import { IProduct } from "@/interfaces";
 
 export default function extractProductAttributes(product: IProduct) {
   const attributes = { title: "", body: [""], footer: "", status: "" };
-  attributes.title = product.descripcion;
-  attributes.body = [`Precio: $${product.precio}`, `Stock: ${product.stockActual}`];
-  attributes.footer = `ID: ${product.id}`;
+  attributes.title = `${product.id} - ${product.nombre}`;
+  attributes.body = [`Descripción: ${product.descripcion}`, `Precio: $${product.precio}`, `Stock actual: ${product.stockActual}`, `Proveedor: ${product.proveedor.nombre} (id=${product.proveedor.id})`];
+  attributes.footer = `Categoría: ${product.categoria.nombre}`;
 
   return attributes;
 }
