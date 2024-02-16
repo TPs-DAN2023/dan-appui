45825d77c57b6a1d02b982056b476df67c6af6f4
ChortJulio <chortjulio@gmail.com>
2024-02-16 11:10:54 -0300

    Fix formatting and remove unused code

--- app/testHome/page.tsx
+++ app/testHome/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { SetStateAction, useEffect, useState } from "react";
 // import { useQuery } from "react-query";
@@ -6,34 +6,27 @@ import { NavBar, Loading } from "@/components";
 import { on } from "events";
 
 const Home = () => {
-
   const myId = 1; // Ideally, this should be retrieved with the actual user ID
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-
     async function fetchData() {
-      console.log('Loading')
-
       // Wait randomly between 300 and 5000 ms for our 'request'
       const wait = Math.floor(Math.random() * 300) + 200;
       await new Promise((p) => setTimeout(p, wait));
-  
-      setIsLoading(false)
+
+      setIsLoading(false);
     }
 
-    fetchData()
+    fetchData();
   }, []);
 
   if (isLoading) return <Loading />;
 
-  if (error) return "Something went wrong: " + error;
-
   return (
     <section className="h-screen flex flex-col">
       <header className="border border-b-blue-300">
-        <NavBar/>
+        <NavBar />
       </header>
       <main className="flex overflow-x-hidden overflow-y-hidden flex-grow">
         <div className="flex flex-col items-center justify-center flex-grow">
