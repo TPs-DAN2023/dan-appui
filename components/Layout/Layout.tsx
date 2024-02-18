6ab5a2665ffcb87f8fb506d5bc4a4b43c485688a
ChortJulio <chortjulio@gmail.com>
2024-02-18 00:45:16 -0300

    Add className prop to Layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,13 +2,14 @@ import NavBar from "../NavBar/NavBar";
 
 interface LayoutProps {
   children: any;
+  className?: string;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children, className }: LayoutProps) {
   return (
-    <section className="h-screen flex flex-col">
+    <section className={`h-screen flex flex-col ${className}`}>
       <header className="border border-b-blue-300">
-        <NavBar/>
+        <NavBar />
       </header>
 
       <main className="flex overflow-x-hidden overflow-y-hidden flex-grow">
