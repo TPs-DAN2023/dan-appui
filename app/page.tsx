9689832e2b18196934446592f1b3a21aeb80942f
ChortJulio <chortjulio@gmail.com>
2024-02-18 19:26:11 -0300

    Update app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
-'use client'
+"use client";
 
-import { Login, CustomButton } from "@/components";
+import { Login, ConfirmButton } from "@/components";
+import Link from "next/link";
 
 export default function Home() {
-
   return (
     <>
       <main className="relative h-screen flex-col flex items-center justify-center">
         <Login />
-      <section className="absolute bottom-4 right-4 flex flex-col gap-y-2 items-center">
-        <h1>Hacer testing de los MS</h1>
-        <CustomButton text="Testear MS" href="/test" />
-      </section>
+        <section className="absolute bottom-4 right-4 flex flex-col gap-y-2 items-center">
+          <h1>Hacer testing de los MS</h1>
+          <Link href="/test">
+            <ConfirmButton onClick={() => {}} className="ml-2">
+              Test MS
+            </ConfirmButton>
+          </Link>
+        </section>
       </main>
     </>
   );
