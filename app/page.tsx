8d94b361e9dc3baf9a7536587175c5157b515c9a
ChortJulio <chortjulio@gmail.com>
2024-02-24 19:48:25 -0300

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,41 @@
 "use client";
 
-import { Login, ConfirmButton } from "@/components";
-import Link from "next/link";
+import { Loading, NavBar } from "@/components";
+import { ROUTES } from "@/constants";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useUser } from "@/hooks";
+import { withAuth } from "@/hocs";
 
-export default function Home() {
+function Home() {
+  const { userLoggedIn, loading } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      if (!loading && !userLoggedIn) {
+        await router.push(ROUTES.LOGIN);
+      }
+    };
+
+    checkAuth();
+  }, [userLoggedIn, loading, router]);
+
+  // if (loading || !userLoggedIn) return <Loading />;
+
   return (
-    <>
-      <main className="relative h-screen flex-col flex items-center justify-center">
-        <Login />
-        <section className="absolute bottom-4 right-4 flex flex-col gap-y-2 items-center">
-          <h1>Hacer testing de los MS</h1>
-          <Link href="/test">
-            <ConfirmButton onClick={() => {}} className="ml-2">
-              Test MS
-            </ConfirmButton>
-          </Link>
-        </section>
+    <section className="h-screen flex flex-col">
+      <header className="border border-b-blue-300">
+        <NavBar />
+      </header>
+      <main className="flex overflow-x-hidden overflow-y-hidden flex-grow">
+        <div className="flex flex-col items-center justify-center flex-grow">
+          <h1 className="text-4xl font-bold">Bienvenido a Marketplace B2B</h1>
+          <p className="text-2xl">Selecciona una opción del menú</p>
+        </div>
       </main>
-    </>
+    </section>
   );
 }
+
+export default withAuth(Home);
