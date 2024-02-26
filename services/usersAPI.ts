44cbd623262c113cecac2d85a439654b787feb9c
ChortJulio <chortjulio@gmail.com>
2024-02-26 18:55:28 -0300

    Add updateUser and deleteUser functions to usersAPI.ts

--- services/usersAPI.ts
+++ services/usersAPI.ts
@@ -18,4 +18,26 @@ const createUser = async (user: any) => {
   return response.json();
 }
 
-export { getUsers, createUser };
+const updateUser = async (user: any) => {
+  const response = await fetch(`${API_URLS.users}/${user.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${getUserToken()}`,
+    },
+    body: JSON.stringify(user),
+  });
+  return response.json();
+}
+
+const deleteUser = async (id: string | undefined) => {
+  const response = await fetch(`${API_URLS.users}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${getUserToken()}`,
+    },
+  });
+  return response.json();
+}
+
+export { getUsers, createUser, updateUser, deleteUser };
