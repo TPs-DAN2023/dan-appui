4df022709824a0e98a6301bc7a4d47559f57b273
ChortJulio <chortjulio@gmail.com>
2024-02-26 18:55:22 -0300

    Refactor extractOrderAttributes function

--- utils/extractOrderAttributes.ts
+++ utils/extractOrderAttributes.ts
@@ -1,12 +1,12 @@
-import { IOrder } from "@/interfaces/IOrder";
+import { IOrder } from "@/interfaces";
 import { formatDate } from "./index"
 
 export default function extractOrderAttributes (order: IOrder) {
   const attributes = {title: '', body: [''], footer: '', status: ''};
-  attributes.title = `ID pedido: ${order.id}`;
-  attributes.body = [`Cantidad de productos: ${order.detallePedido && order.detallePedido.length}`, `Precio total: $${order.detallePedido && order.detallePedido.reduce((acc, item) => acc + item.precio, 0)}`, `Proveedor: ${order.proveedor.nombre} (id=${order.proveedor.id})`, `Usuario: ${order.user.userName} (id=${order.user.id})`, `Fecha: ${formatDate(order.fecha)}`];
-  attributes.footer = `Observaciones: ${order.observaciones}`;
-  attributes.status = `${order.estados && order.estados[order.estados.length - 1].estado	}`
+  attributes.title = `ID: ${order.id}`;
+  attributes.body = [`Cantidad de órdenes: ${order.detallePedido && order.detallePedido.length}`, `Precio total: $${order.total}`, `Cliente: ${order.cliente.razonSocial} (id=${order.cliente.id})`, `Usuario: ${order.user}`, `Fecha: ${formatDate(order.fecha)}`];
+  attributes.footer = `Observaciones: ${order.observaciones}, Num. pedido: ${order.numeroPedido}`;
+  attributes.status = `${order.estados && order.estados[order.estados.length - 1].estado}`
 
   return attributes;
 }
