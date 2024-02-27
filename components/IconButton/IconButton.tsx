880238068a21b563264e19e91ae7a0ac08df6f28
ChortJulio <chortjulio@gmail.com>
2024-02-27 18:00:35 -0300

    Add label to IconButton component

--- components/IconButton/IconButton.tsx
+++ components/IconButton/IconButton.tsx
@@ -3,25 +3,30 @@
 interface IconButtonProps {
   onClick: VoidFunction;
   children: React.ReactNode;
+  label?: string;
   disabled?: boolean;
 }
 
 export default function IconButton({
   onClick,
   children,
+  label,
   disabled,
 }: IconButtonProps) {
   return (
-    <button
-      className={`rounded-3xl self-end ${
-        disabled
-          ? "cursor-not-allowed"
-          : "cursor-pointer hover:scale-125 hover:opacity-100 transition-all  "
-      }`}
-      onClick={() => onClick()}
-      disabled={disabled}
-    >
-      {children}
-    </button>
+    <article className="flex flex-col justify-center">
+      <button
+        className={`rounded-3xl ${
+          disabled
+            ? "cursor-not-allowed"
+            : "cursor-pointer hover:scale-125 hover:opacity-100 transition-all  "
+        }`}
+        onClick={() => onClick()}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+      {label && <label htmlFor="from">{label}</label>}
+    </article>
   );
 }
