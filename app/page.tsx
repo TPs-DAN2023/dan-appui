202d2c6d2ffdf8214f18d43a390c00e705fb0122
ChortJulio <chortjulio@gmail.com>
2024-02-27 18:01:19 -0300

    Update home app text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { Loading, NavBar } from "@/components";
+import { USER_TYPES } from "@/constants";
 import { withAuth } from "@/hocs";
 import { useUser } from "@/hooks";
+import { hasUserType } from "@/utils";
 
 function Home() {
   const { loading } = useUser();
@@ -15,9 +17,13 @@ function Home() {
         <NavBar />
       </header>
       <main className="flex overflow-x-hidden overflow-y-hidden flex-grow">
-        <div className="flex flex-col items-center justify-center flex-grow">
+        <div className="flex flex-col items-center justify-center flex-grow text-center">
           <h1 className="text-4xl font-bold">Bienvenido a Marketplace B2B</h1>
-          <p className="text-2xl">Selecciona una opción del menú</p>
+          <p className="text-2xl max-w-[800px] text-balance mt-4">
+            {hasUserType(USER_TYPES.ADMIN)
+              ? "Gestione usuarios, productos y cambie los estados de los pedidos realizados por los clientes rápido, seguro y sin vueltas!"
+              : "Desde aquí podrá ver los productos disponibles y realizar pedidos de manera rápida, segura y sin vueltas!"}
+          </p>
         </div>
       </main>
     </section>
