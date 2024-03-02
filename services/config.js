9c715bd84ab071169df9f760523d318a6327b002
ChortJulio <chortjulio@gmail.com>
2024-03-02 11:17:16 -0300

    Update API_URLS with new paths

--- services/config.js
+++ services/config.js
@@ -1,14 +1,13 @@
 export const API_BASE_URL = 'http://localhost/api';
 
 export const API_URLS = {
-  categories: `${API_BASE_URL}/categorias`,
+  categories: `${API_BASE_URL}/productos/categorias`,
   login: `${API_BASE_URL}/auth/login`,
   orders: `${API_BASE_URL}/pedidos`,
   products: `${API_BASE_URL}/productos`,
-  providers: `${API_BASE_URL}/proveedores`,
+  providers: `${API_BASE_URL}/productos/proveedores`,
   users: `${API_BASE_URL}/usuarios`,
-  userTypes: `${API_BASE_URL}/tipos-usuario`,
-  validateToken: `${API_BASE_URL}/auth/validate`,
+  userTypes: `${API_BASE_URL}/usuarios/tipos`,
 };
 
 export default API_URLS;
